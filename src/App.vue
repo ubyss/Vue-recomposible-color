4a1ab26ca161e6c4784304f8e9c9677ea6d11035
Thiago Vinícius <80261904+ubyss@users.noreply.github.com>
2022-03-16 11:20:32 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <label
+      >Cor em Hexadecimal:
+      <input type="text" v-model="cor" />
+    </label>
+    <Quadrado :cor="cor" />
+  </div>
+</template>
+
+<script>
+import Quadrado from "./components/Quadrado";
+
+export default {
+  data() {
+    return {
+      cor: "000000",
+    };
+  },
+  name: "App",
+  components: {
+    Quadrado,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Quadrado.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <div class="bloco" :style="{ background: '#' + cor }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quadrado",
+  props: {
+    msg: String,
+    cor: String,
+  },
+};
+</script>
+
+
+<style>
+.bloco {
+  width: 200px;
+  height: 200px;
+  /* background: #000000; */
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
